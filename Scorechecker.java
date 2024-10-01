570403225309f8a6de7b5b53af356d358cbb6c5d
HEMRON ROSENDRICK BAUTISTA ILAO <PRG1-26@ITDOMAIN.AUP.EDU.PH>
2024-10-01 10:08:04 +0800

    added percentage

--- Scorechecker.java
+++ Scorechecker.java
@@ -1,25 +1,35 @@
 import java.util.Scanner;
 
-public class Scorechecker{
-    public static void main(String[] args){
+public class Scorechecker {
+    public static void main(String[] args) {
         Scanner drone = new Scanner(System.in);
         System.out.print("Input the maximum score: ");
-        int maxscore = drone.nextInt();
-        
+        double maxscore = drone.nextDouble();
+
         System.out.print("Input your score: ");
-        int score = drone.nextInt();
-        
+        double score = drone.nextDouble();
+
         if (score == maxscore) {
-            System.out.println("You Got a Perfect Score!!!"); 
-            } else if (score < (maxscore-1) && score > 0) {
-                System.out.println("You made " + (maxscore-score) + " mistakes. You can do better than that!");
-            } else if (score == (maxscore-1)) { 
-                System.out.println("Ooooh... so close. you made just one mistake.\nits okay, you'll get that perfect score next time.");
-            } else if (score < 0) {
-                System.out.println("How did you get a negative score? were you caught cheating??");
-            } else if (score > maxscore) {
-                System.out.println("Wow! You got " + (score-maxscore) + " points higher than the maximum score of  " + maxscore + "!\nDid you bribe the examiner?");
-            }
-    // :> fklajlfsjkflsf
-}
+            System.out.println("You Got a Perfect Score!!!");
+        } else if (score < (maxscore - 1) && score > 0) {
+            System.out.println("You made " + (maxscore - score) + " mistakes. You can do better than that!");
+        } else if (score == (maxscore - 1)) {
+            System.out.println(
+                    "Ooooh... so close. you made just one mistake.\nits okay, you'll get that perfect score next time.");
+        } else if (score < 0) {
+            System.out.println("How did you get a negative score? were you caught cheating??");
+        } else if (score > maxscore) {
+            System.out.println("Wow! You got " + (score - maxscore) + " points higher than the maximum score of  "
+                    + maxscore + "!\nDid you bribe the examiner?");
+        }
+        Scanner commands = new Scanner(System.in);
+        System.out.print("Enter a command: ");
+        String command = commands.nextLine();
+
+        if (command.equals("percentage")) {
+            System.out.print("You got " + String.format("%.2f", ((score / maxscore) * 100)) + "%");
+        } else {
+            System.out.print("unknown command.");
+        }
+    }
 }
