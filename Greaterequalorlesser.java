a82145b99b2bd8a55ccd791a5f644a598e88f74e
HEMRON ROSENDRICK BAUTISTA ILAO <INTC-24@ITDOMAIN.AUP.EDU.PH>
2024-10-08 12:46:40 +0800

    hehe do while is dabest

---
+++ Greaterequalorlesser.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Greaterequalorlesser{
+    public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    Scanner in2 = new Scanner(System.in);
+    String command = "";
+   
+    do{
+    System.out.println("Type \"start\" to start the program, type \"quit\" to terminate it.");
+    command = in2.nextLine();
+    
+    if (command.equals("start")) {
+    System.out.print("enter a number: ");
+    int num1 = in.nextInt();
+    System.out.print("enter another number: ");
+    int num2 = in.nextInt();
+    
+    if (num1 == num2){
+        System.out.println("the 2nd number is equal to the first");
+        System.out.println(" ");
+    } else if (num1 > num2) {
+        System.out.println("the 2nd number is less than the first");
+        System.out.println(" ");
+    } else {
+        System.out.println("the 2nd number is greater than the first.");
+        System.out.println(" ");
+        
+    }
+        
+    } else if (command.equals("quit")) 
+        {System.out.print("quitting...");
+        } else { 
+            System.out.println("error");
+            System.out.println(" ");
+    }
+    } while (!command.equals("quit"));
+    }
+    
+}
