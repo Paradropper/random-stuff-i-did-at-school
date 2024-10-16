06a0d6e5da65ea0e8db27dd39672364e832d1bcb
HEMRON ROSENDRICK BAUTISTA ILAO <INTC-24@ITDOMAIN.AUP.EDU.PH>
2024-10-16 10:57:37 +0800

    writes changed directory to file

--- changedirectorymodifier.java
+++ changedirectorymodifier.java
@@ -1,21 +1,34 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class changedirectorymodifier {
     public static void main(String[] args){
 
     Scanner in = new Scanner(System.in);
 
+    String address = "";
+do {
     System.out.print("Enter the address to convert: ");
-    String address = in.nextLine();
+    address = in.nextLine();
+
+    if (!address.equals("return")) {
 
     String modifiedaddress = address.replace("\\", "/");
     modifiedaddress = modifiedaddress.replace(":", "");
     modifiedaddress = "/" + modifiedaddress;
 
-    System.out.println("");
-    System.out.println(modifiedaddress);
-    System.out.println("");
 
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter("random-stuff-i-did-at-school/modifiedadressoutput.txt"))) { 
+     writer.write(modifiedaddress);   
+     System.out.println("Address successfully writen to file: \"" + modifiedaddress + "\"");
+    } catch (IOException e) {
+        System.err.println("Error writing to file: " + e.getMessage());
+    }
+} 
+} while (!address.equals("return"));
     in.close();
     }
 }
